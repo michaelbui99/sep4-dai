660bf5ee614f148df065a1d798f26cdb312a09a2
LilianBittar <bittarlily@gmail.com>
2022-05-05 12:18:00 +0200

    implemented Rooms Controller class

--- WebAPI/Controllers/RoomsController.cs
+++ WebAPI/Controllers/RoomsController.cs
@@ -1,6 +1,38 @@
-﻿namespace WebAPI.Controllers
+﻿using System.Collections;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Repositories;
+using WebAPI.Services;
+
+namespace WebAPI.Controllers
 {
-    public class RoomsController
+    [ApiController]
+    [Route("[controller]")]
+    public class RoomsController : ControllerBase
     {
+        private IRoomService roomService;
+
+        public RoomsController(IRoomService roomService)
+        {
+            this.roomService = roomService;
+        }
+
+        [HttpGet]
+    
+        public async Task<ActionResult<IEnumerable<Measurement>>> GetMeasurmentByRoomId([FromQuery] int RoomId) {
+            try
+            {
+                IEnumerable<Measurement> measurements = await roomService.GetMeasurementsByRoomIdAsync(RoomId);
+                return Ok(measurements);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
     }
+    
+    
+
 }
