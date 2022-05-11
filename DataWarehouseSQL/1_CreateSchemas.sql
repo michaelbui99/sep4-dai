b764c16644cfefe4bd1f9387cb6b7273d0e42af8
Michael Bui <michaelbui99@hotmail.com>
2022-05-11 12:54:19 +0200

    Added directory for SQL files for DWH

---
+++ DataWarehouseSQL/1_CreateSchemas.sql
@@ -0,0 +1,15 @@
+use sep4_source 
+go
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'stage')
+EXEC('CREATE SCHEMA stage')
+go
+
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'edw')
+EXEC('CREATE SCHEMA edw')
+go
+
+
+
+
