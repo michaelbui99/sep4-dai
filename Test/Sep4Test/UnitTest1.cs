f13d3e93a479204ff93162c65615ea944c8c4e4d
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2022-05-11 13:45:47 +0200

    Test Project Created

---
+++ Test/Sep4Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Sep4Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
