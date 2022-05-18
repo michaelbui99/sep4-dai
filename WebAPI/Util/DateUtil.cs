ee966c916d86ee61302a27691e6a89ab8f795f29
Michael Bui <michaelbui99@hotmail.com>
2022-05-18 17:57:08 +0200

    Added util class for converting unix time seconds to date time

---
+++ WebAPI/Util/DateUtil.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI.Util;
+
+public class DateUtil
+{
+    public static DateTime GetDateTimeFromUnixTimeSeconds(long unixTimeSeconds)
+    {
+        var dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(unixTimeSeconds);
+        return dateTimeOffset.DateTime;
+    }
+}
