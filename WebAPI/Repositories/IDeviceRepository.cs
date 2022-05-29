5ef79ad1b97e074edb84f573a4e3f4be680a1b4d
Kasper-S-Jensen <kasperscoop@gmail.com>
2022-05-29 10:36:51 +0200

    added documentation to interfaces

--- WebAPI/Repositories/IDeviceRepository.cs
+++ WebAPI/Repositories/IDeviceRepository.cs
@@ -4,8 +4,23 @@ namespace WebAPI.Repositories
 {
     public interface IDeviceRepository
     {
+        /// <summary>
+        ///  gets all devices
+        /// </summary>
+        /// <returns> a list of climatedevices</returns>
         Task<IEnumerable<ClimateDevice>> GetAllDevicesAsync();
+        /// <summary>
+        /// get a device by Id
+        /// </summary>
+        /// <param name="deviceId"> the  id of the device you want returned</param>
+        /// <returns> a climatedevice</returns>
         Task<ClimateDevice> GetDeviceByIdAsync(string deviceId);
+        /// <summary>
+        ///  adds a new device
+        /// </summary>
+        /// <param name="device"> the device object you want added</param>
+        /// <exception cref="ArgumentException"> error on insert query</exception>
+        /// <returns> void</returns>
         Task AddNewDeviceAsync(ClimateDevice device);
     }
 }
--- WebAPI/Services/IDeviceService.cs
+++ WebAPI/Services/IDeviceService.cs
@@ -4,9 +4,31 @@ namespace WebAPI.Services
 {
     public interface IDeviceService
     {
+        /// <summary>
+        /// fetches list of device names
+        /// </summary>
+        /// <returns> a list of all existing climatedevice names</returns>
         Task<IDictionary<string, string>> GetRoomNamesForDevices();
+        /// <summary>
+        ///  gets all devices
+        /// </summary>
+        /// <returns> a list of climatedevices</returns>
         Task<IEnumerable<ClimateDevice>> GetAllDevicesAsync();
+        /// <summary>
+        /// get a device by Id
+        /// </summary>
+        /// <param name="deviceId"> the  id of the device you want returned</param>
+        /// <exception cref="ArgumentNullException"> on null argument</exception>
+        /// <exception cref="ArgumentException"> if device does not exist</exception>
+        /// <returns> a climatedevice</returns>
         Task<ClimateDevice> GetDeviceByIdAsync(string deviceId);
+        /// <summary>
+        ///  adds a new device
+        /// </summary>
+        /// <param name="device"> the device object you want added</param>
+        /// <exception cref="DeviceAlreadyExistsException"> on aslready exists</exception>
+        /// <exception cref="ArgumentNullException"> on null argument</exception>
+        /// <returns> void</returns>
         Task AddNewDeviceAsync(ClimateDevice device);
     }
 }
